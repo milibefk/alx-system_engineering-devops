954c8585bafa4adc32be133316923a3a3c307349
milibefk <99682095+milibefk@users.noreply.github.com>
2022-09-16 17:17:45 +0300

    Ggh

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,16 @@
-# creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# create a file in /tmp.
+file { '/tmp/school':
+ensure  =>'file',
+path    =>'/tmp/school',
+mode    =>'0744',
+owner   =>'www-data',
+group   =>'www-data',
+content => 'I love Puppet'
 }
+
+
+
+
+
+
+
